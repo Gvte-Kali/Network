6466ffb098e680cb39d65eb4798d4033e367b715
b1g_ph4rm4 <132758165+Gvte-Kali@users.noreply.github.com>
2025-01-24 18:11:41 +0100

    Update Step1.sh

--- Easy_PiVPN/Step1.sh
+++ Easy_PiVPN/Step1.sh
@@ -3,10 +3,10 @@ GRAY_BLUE="\033[1;34m"
 LIGHT_BLUE="\033[1;36m"
 NC="\033[0m"         
 
-clear
 
 # Step 1: Update packages
 step1() {
+  clear
   echo -e "\n${GRAY_BLUE}=== Step 1: Updating packages ===${NC}"
   sudo apt update && sudo apt upgrade -y
   echo
