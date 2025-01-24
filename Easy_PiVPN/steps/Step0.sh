f98c76d8d6821bbbac298eae41feaea4c9c6c4b6
b1g_ph4rm4 <132758165+Gvte-Kali@users.noreply.github.com>
2025-01-24 18:51:15 +0100

    Update Step0.sh

--- Easy_PiVPN/steps/Step0.sh
+++ Easy_PiVPN/steps/Step0.sh
@@ -10,15 +10,15 @@ step0() {
   echo -e "\n${GRAY_BLUE}=== Step 0: Preparing directories ===${NC}"
   
   # Create the vpn_config and public_cron folders
-  mkdir -p "$HOME/vpn_config"
+  mkdir -p "/home/$USER/vpn_config"
   
   # Create log and temporary files if necessary
-  touch "$HOME/vpn_config/install_log.txt"
-  touch "$HOME/vpn_config/temp_config.txt"
+  touch "/home/$USER/vpn_config/install_log.txt"
+  touch "/home/$USER/vpn_config/temp_config.txt"
   
   # Retrieve the public IP address
   public_ip=$(curl -s ifconfig.me)
-  echo "$public_ip" > "$HOME/vpn_config/public_ip"
+  echo "$public_ip" > "/home/$USER/vpn_config/public_ip"
 
   echo "Directories and configuration files created."
   echo
