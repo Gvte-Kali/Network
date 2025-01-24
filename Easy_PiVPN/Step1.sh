0fdde845bb47e419981973f3a4382e9e2948eada
b1g_ph4rm4 <132758165+Gvte-Kali@users.noreply.github.com>
2025-01-24 18:11:05 +0100

    Update Step1.sh

--- Easy_PiVPN/Step1.sh
+++ Easy_PiVPN/Step1.sh
@@ -3,6 +3,8 @@ GRAY_BLUE="\033[1;34m"
 LIGHT_BLUE="\033[1;36m"
 NC="\033[0m"         
 
+clear
+
 # Step 1: Update packages
 step1() {
   echo -e "\n${GRAY_BLUE}=== Step 1: Updating packages ===${NC}"
