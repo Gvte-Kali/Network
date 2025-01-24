6f71210fe9ced974403c3dc362661cfe2a44e561
b1g_ph4rm4 <132758165+Gvte-Kali@users.noreply.github.com>
2025-01-24 14:21:29 +0100

    Create Step4.sh

---
+++ Easy_PiVPN/Step4.sh
@@ -0,0 +1,31 @@
+# Define colors
+GRAY_BLUE="\033[1;34m"    # 
+LIGHT_BLUE="\033[1;36m"   # 
+NC="\033[0m"              # Reset color
+
+# Step 4: Display detailed network information
+step4() {
+  echo -e "\n${GRAY_BLUE}=== Step 4: Network Information ===${NC}"
+  
+  # Using the ip command and resolvconf command to retrieve information
+  ip -br addr | while read -r interface status ip_info; do
+    # Extracting the IP address and subnet mask
+    ip_address=$(echo "$ip_info" | cut -d'/' -f1)
+    netmask=$(echo "$ip_info" | cut -d'/' -f2)
+    
+    # Retrieving the gateway
+    gateway=$(ip route | grep default | awk '{print $3}')
+    
+    # Retrieving the DNS servers
+    dns_servers=$(grep -m 1 '^nameserver' /etc/resolv.conf | awk '{print $2}')
+    
+    # Formatted output
+    echo -e "${LIGHT_BLUE}Interface${NC}   : $interface"
+    echo -e "IP Address${NC} : $ip_address/$netmask"
+    echo -e "Gateway${NC} : $gateway"
+    echo -e "DNS       : $dns_servers"
+    echo "---"
+  done
+  
+  echo
+}
