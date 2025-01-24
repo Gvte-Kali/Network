fc120c8537f1a1458f372c591b9eed33dc73d767
b1g_ph4rm4 <132758165+Gvte-Kali@users.noreply.github.com>
2025-01-24 18:11:53 +0100

    Update Step0.sh

--- Easy_PiVPN/Step0.sh
+++ Easy_PiVPN/Step0.sh
@@ -3,10 +3,10 @@ GRAY_BLUE="\033[1;34m"
 LIGHT_BLUE="\033[1;36m"
 NC="\033[0m"   
 
-clear
 
 # Step 0: Preparing directories and configuration files
 step0() {
+  clear
   echo -e "\n${GRAY_BLUE}=== Step 0: Preparing directories ===${NC}"
   
   # Create the vpn_config and public_cron folders
