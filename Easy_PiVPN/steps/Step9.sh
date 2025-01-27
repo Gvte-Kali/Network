33d2fe21074170eb837cf1125d197636f212f7fc
b1g_ph4rm4 <132758165+Gvte-Kali@users.noreply.github.com>
2025-01-27 14:42:50 +0100

    Create Step9.sh

---
+++ Easy_PiVPN/steps/Step9.sh
@@ -0,0 +1,19 @@
+# Colors
+GRAY_BLUE="\033[1;34m"    # Dark gray blue
+LIGHT_BLUE="\033[1;36m"   # Light blue
+RED="\033[1;31m"          # Red
+GREEN="\033[1;32m"        # Green
+YELLOW="\033[1;33m"       # Yellow
+MAGENTA="\033[1;35m"      # Magenta
+CYAN="\033[1;36m"         # Cyan
+WHITE="\033[1;37m"        # White
+NC="\033[0m"              # Reset color
+
+# Step 9: Install PiVPN
+step9() {
+  echo -e "\n${GRAY_BLUE}=== Step 9: PiVPN Installation ===${NC}"
+  curl -L https://install.pivpn.io | bash
+  echo
+}
+
+Step9
